b5ae740182bd746ac572209062d74c0ffdd915be
Joaquin Diaz <diaz.joaquin.e@gmail.com>
2024-05-05 19:19:48 -0300

    Add Validations to User Service

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,14 +1,41 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { 
+  Body, 
+  Controller, 
+  Delete, 
+  Get, 
+  Param, 
+  ParseIntPipe, 
+  Patch, 
+  Post } from '@nestjs/common';
 import { CreateUserDto } from './dto/createUser.dto';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
-
   constructor(private usersService: UsersService) {}
 
   @Post()
   createUser(@Body() newUser: CreateUserDto) {
     return this.usersService.createUser(newUser);
+  }
+
+  @Get()
+  getUsers() {
+    return this.usersService.getUsers();
+  }
+
+  @Get(':id')
+  getUser(@Param('id', ParseIntPipe) id: number) {
+    return this.usersService.getUser(id);
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id', ParseIntPipe) id: number) {
+    this.usersService.deleteUser(id)
+  }
+  
+  @Patch(':id')
+  updateUser(@Param('id', ParseIntPipe) id: number, @Body() user: CreateUserDto) {
+    return this.usersService.updateUser(id, user);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,16 +1,62 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
 import { CreateUserDto } from './dto/createUser.dto';
+import { UpdateUserDto } from './dto/updateUser.dto';
 
 @Injectable()
 export class UsersService {
-
   constructor(@InjectRepository(User) private userRepository: Repository<User>) {}
   
-  createUser(user: CreateUserDto) {
+  async createUser(user: CreateUserDto) {
+
+    const userFound = await this.userRepository.findOne({
+      where: {
+        username: user.username
+      }
+    });
+
+    if (userFound) {
+      return new HttpException('User already exists', HttpStatus.CONFLICT);
+    }
     const newUser = this.userRepository.create(user);
     this.userRepository.save(newUser);
+  }
+
+  getUsers() {
+    return this.userRepository.find();
+  }
+
+   async getUser(id: number) {
+    const userFound = await this.userRepository.findOne({ where: { id } });
+
+    if (!userFound) {
+      return new HttpException('User not found', HttpStatus.NOT_FOUND);
+    }
+
+    return userFound;
+  }
+
+  async deleteUser(id: number) {
+    const userFound = await this.userRepository.findOne({ where: { id } });
+
+    if (!userFound) {
+      return new HttpException('User not found', HttpStatus.NOT_FOUND);
+    }
+
+    return this.userRepository.delete({ id });
+  }
+
+  async updateUser(id: number, user: UpdateUserDto) {
+
+    const userFound = await this.userRepository.findOne({ where: { id } });
+
+    if (!userFound) {
+      return new HttpException('User not found', HttpStatus.NOT_FOUND);
+    }
+
+    const updateUser = Object.assign(userFound, user);
+    return this.userRepository.save(updateUser);
   }
 }
