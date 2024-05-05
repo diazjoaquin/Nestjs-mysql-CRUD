422a5e1e55855cd4779f39ab4948443dec31b80d
Joaquin Diaz <diaz.joaquin.e@gmail.com>
2024-05-05 18:31:06 -0300

    Create users Module

---
+++ src/users/users.controller.ts
@@ -0,0 +1,14 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dto/createUser.dto';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+
+  constructor(private usersService: UsersService) {}
+
+  @Post()
+  createUser(@Body() newUser: CreateUserDto) {
+    return this.usersService.createUser(newUser);
+  }
+}
